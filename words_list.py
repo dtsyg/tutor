470edd1dcf5ea2c6d1aa94a2ef1a5b808b7fd5df
dtsyg <daim.tsyg@gmail.com>
2016-04-23 20:45:40 +0500

    added a structure @ words_list_v2

--- words_list.py
+++ words_list.py
@@ -2,32 +2,68 @@
 program printed all words in list (random sort)
 all words, one word -- one print
 v1 // simple
-v2 // with user-input list
+v2 // with user-input words-list and prepared words-list
+v3 // with auto-generate words-list
 """
 
 import random
 
 # parametrization
 WORDS = ("one", "two", "two", "six", "three", "four", "five", "five")
+user_words = []
+user_word = " "
 uniq = []
 
-print("""
-old words-list: """, WORDS)
+# user input
+user_choice = input(
+    """
+    whether you want himself type the words-list? Y / N
+    """)
 
-# remove duplicates
-for word in WORDS:
-    if word not in uniq:
-        uniq.append(word)
+# user-input words-list
+if user_choice.upper == "Y":
+    # user-input words-list
+    while user_word:
+        user_word = input(
+            """
+            please type the word,
+            if you have finished -- press enter-key without word
+            """)
+        user_words.append(user_word)
 
-# shuffle
-random.shuffle(uniq)
+    print("""
+    old words-list: """, user_words)
 
+    # shuffle
+    random.shuffle(user_words)
 
-# output
-print("""
-new words-list: """, uniq)
+    # output
+    print("""
+    new words-list: """, user_words)
 
-input("""
+# prepared words-list
+elif user_choice.upper == "N":
+    print("""
+    old words-list: """, WORDS)
 
-press enter-key to exit
-""")
+    # remove duplicates
+    for word in WORDS:
+        if word not in uniq:
+            uniq.append(word)
+
+    # shuffle
+    random.shuffle(uniq)
+
+    # output
+    print("""
+    new words-list: """, uniq)
+
+# implicit user_choice
+else:
+    print("sorry, but your choice implicit, try again;")
+    user_choice = input(
+        """
+        whether you want himself enter a words-list? Y / N
+        """)
+
+input("\npress enter-key to exit")
