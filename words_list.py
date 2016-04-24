b1098a8446116cc43bf74797bac82c65a40e7ff8
dtsyg <daim.tsyg@gmail.com>
2016-04-25 00:35:36 +0500

    complete @ words_list_v2

--- words_list.py
+++ words_list.py
@@ -3,7 +3,7 @@ program printed all words in list (random sort)
 all words, one word -- one print
 v1 // simple
 v2 // with user-input words-list and prepared words-list
-v3 // with auto-generate words-list
+v3 // with auto-generate words-list, (and with user-input words-list and prepared words-list -- menu)
 """
 
 import random
@@ -12,16 +12,19 @@ import random
 WORDS = ("one", "two", "two", "six", "three", "four", "five", "five")
 user_words = []
 user_word = " "
-uniq = []
+uniq_prep = []
+uniq_user = []
 
 # user input
 user_choice = input(
     """
     whether you want himself type the words-list? Y / N
     """)
+user_choice.lower
 
 # user-input words-list
-if user_choice.upper == "Y":
+if user_choice == "y":
+
     # user-input words-list
     while user_word:
         user_word = input(
@@ -30,33 +33,37 @@ if user_choice.upper == "Y":
             if you have finished -- press enter-key without word
             """)
         user_words.append(user_word)
-
     print("""
     old words-list: """, user_words)
 
+    # remove duplicates
+    for word in user_words:
+        if word not in uniq_user:
+            uniq_user.append(word)
+
     # shuffle
-    random.shuffle(user_words)
+    random.shuffle(uniq_user)
 
     # output
     print("""
-    new words-list: """, user_words)
+    new words-list: """, uniq_user)
 
 # prepared words-list
-elif user_choice.upper == "N":
+elif user_choice == "n":
     print("""
     old words-list: """, WORDS)
 
     # remove duplicates
     for word in WORDS:
-        if word not in uniq:
-            uniq.append(word)
+        if word not in uniq_prep:
+            uniq_prep.append(word)
 
     # shuffle
-    random.shuffle(uniq)
+    random.shuffle(uniq_prep)
 
     # output
     print("""
-    new words-list: """, uniq)
+    new words-list: """, uniq_prep)
 
 # implicit user_choice
 else:
